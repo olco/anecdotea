c2c365bdcdd103bcc540534eff726d0db34f1fc8
cosquer <o.cosquer@systel-fr.com>
2016-09-22 17:01:29 +0200

    feat(tech):Test tri + ajout images

--- www/js/services.js
+++ www/js/services.js
@@ -8,27 +8,32 @@ angular.module('starter.services', ['ngCordova'])
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'img/ben.png'
+    face: 'img/transbordeur.jpg',
+    dist: 100
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    face: 'img/max.png',
+    dist: 600
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    face: 'img/corderie.jpg',
+    dist: 150
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: 'img/perry.png',
+    dist: 101
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: 'img/mike.png',
+    dist: 1000
   }];
 
   return {
